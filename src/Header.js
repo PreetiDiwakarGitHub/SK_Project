9daae19f78126474b58176004c1e83be3993fc8c
Preeti <preetidiwakar23@navgurukul.org>
2025-03-27 00:15:31 +0530

    logo

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
+import logo from "../src/image/Logo.jpg"; // Make sure the path is correct
+
 function Header() {
   return (
     <header className="bg-blue-600 text-white py-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <div className="text-2xl font-bold">SK Enterprises</div>
+        
+        {/* Logo with Text */}
+        <div className="logo-container">
+          <img src={logo} alt="SK Enterprises Logo" className="logo-img" />
+          <span className="logo-text">SK Enterprises</span>
+        </div>
+
+        {/* Navigation */}
         <nav>
           <ul className="flex space-x-6">
             <li><Link to="/" className="hover:underline">Home</Link></li>
@@ -14,11 +23,12 @@ function Header() {
             <li><Link to="/gallery" className="hover:underline">Gallery</Link></li>
             <li><Link to="/contact" className="hover:underline">Contact Us</Link></li>
             <button className="login-btn">Login</button>
-            <button className="login-btn" >Sing Up</button>
+            <button className="login-btn">Sign Up</button>
           </ul>
         </nav>
       </div>
     </header>
   );
 }
+
 export default Header;
